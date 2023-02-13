c2464f08e0093ef591d2472c440259a43fab6801
“Valerij-Hering” <zell9113@gmail.com>
2023-02-13 23:46:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import girl from './girl.png';
+
 
 function App() {
+
+  const [tips, setTips] = useState('');
+
+  const fetchTips = async () => {
+    const response = await fetch(
+      "http://www.boredapi.com/api/activity/"
+    );
+    const data = await response.json();
+    setTips(data.activity);
+  }
+
+  useEffect(() => {
+    fetchTips();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <div className='fon'></div> */}
+    <div className='container'>
+      <img className='girl-img' src={girl} alt="girl"/>
+      <div className='box-par'>
+        <i className='fa-envelope	fas icon'></i>
+        <p>{tips}</p>
+        <div className='check'>
+          <i class="fi fi-bs-check"></i>
+        </div>
+      </div>
+    </div>
+    <h3>Tips For Boredom</h3>
+    <button className='btn' onClick={fetchTips}>New Tip</button>
     </div>
   );
 }
